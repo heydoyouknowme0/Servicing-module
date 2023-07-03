ee1df66b9f301423bc7fd66dbb9ca07b11a9902b
Antriksh Kumar <68548165+heydoyouknowme0@users.noreply.github.com>
2023-07-03 10:03:38 +0530

    old
    l

--- servicingmodule/src/App.tsx
+++ servicingmodule/src/App.tsx
@@ -3,9 +3,10 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import Header from "./components/Header";
+import Login from "./components/Login";
 
 function App() {
-  return <Header />;
+  return <Login />;
 }
 
 export default App;
